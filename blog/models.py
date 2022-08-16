5ffe7f18188931b833692c18305a7871c46fd506
QAM <qam600@gmail.com>
2022-08-16 16:00:09 +0400

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, date
-
-from django.urls import reverse
 from ckeditor.fields import RichTextField
+from django.urls import reverse
+
 
 
 
@@ -25,13 +25,13 @@ class Blog(models.Model):
     title_tag = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=200)
-    description = RichTextField(null = True)
+    description = RichTextField()
     date = models.DateTimeField(auto_now_add=True)
     
     
 
-    def __unicode__(self):
-        return self.title + '|' + self.author
+    def __str__(self):
+        return f"{self.title} + '|' + {self.author}"
 
     def get_absolute_url(self):
         return reverse('all_blogs')
