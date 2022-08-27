80748482cb13c392a0454b215446f241909d2054
QAM <qam600@gmail.com>
2022-08-27 16:50:23 +0400

    Update views.py

--- apps/views.py
+++ apps/views.py
@@ -8,9 +8,9 @@ import random
 def qrCode(request):
     if request.method == "POST":
         Url = request.POST['url']
-        img = make(Url)
+        img = str(make(Url))
         img_name = f'qrimg{random.randint(1, 1000)}.png'
-        str(img.save(settings.MEDIA_ROOT/img_name))
+        img.save(settings.MEDIA_ROOT/img_name)
         return render(request, 'apps/qrcode.html', {'img':img_name})
     
     return render(request,'apps/qrcode.html')
