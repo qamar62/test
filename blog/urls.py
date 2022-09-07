56231b17b3702df8e24ab82872b05adc18275c51
QAM <qam600@gmail.com>
2022-09-07 17:05:01 +0400

    blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import BlogView, BlogDetailView, BlogCreateView, BlogUpdateView, Blo
 
 urlpatterns = [
     path('', BlogView.as_view(), name='all_blogs'),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name='detail'),
+    path('detail/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
     path('add_post', BlogCreateView.as_view(), name='add_post'),
     path('detail/edit/<int:pk>', BlogUpdateView.as_view(), name='update_post'),
     path('detail/<int:pk>/remove', BlogDeleteView.as_view(), name='delete_post'),
