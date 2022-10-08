bfe9c30f1260d02bd7e3a38a4573560d7838ad8c
QAM <54406289+qamar62@users.noreply.github.com>
2022-10-08 23:09:13 +0400

    blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, date
@@ -11,6 +12,8 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+
 
     def __str__(self):
         return self.name
@@ -26,9 +29,14 @@ class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=200)
     description = RichTextField()
+    image = models.ImageField(upload_to='blog', default='/blog/default.png')
     date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     
     
+    
+
 
     def __str__(self):
         return f"{self.title} + '|' + {self.author}"
