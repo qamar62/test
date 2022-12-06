2ad873c19b3a2ab8a59ee8c3d12b7898ee415086
QAM <54406289+qamar62@users.noreply.github.com>
2022-12-06 23:26:04 +0400

    login

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainSite, name="home")
+    path('', views.mainSite, name="home"),
+    path('contact-us', views.contact, name="contact-us"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -7,3 +7,9 @@ def mainSite(request):
     
     context = {}
     return render(request, 'frontend/seo.html', context)
+
+
+def contact(request):
+    
+    context = {}
+    return render(request, 'frontend/contact_us.html', context)
